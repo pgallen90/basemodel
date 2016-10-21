8e7d1ee58a960b2aa4a78130d8949be2554254ab
Patrick Allen <patrickgallen@gmail.com>
2016-10-21 15:23:46 -0400

    fixed

--- base_model/__init__.py
+++ base_model/__init__.py
@@ -4,20 +4,8 @@ from sqlalchemy.ext.declarative import declared_attr
 
 from flaskapp.extensions import db
 
-def to_snake_case(camel_case):    
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', camel_case)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
-
 
 class BaseModelMixin(object):
-
-    @declared_attr
-    def __tablename__(cls):
-        """Convert CamelCase model (Class) names into something sutable for a 
-        database table name.  
-        """
-        return to_snake_case(cls.__name__)
-
     id =  db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, default=func.now(), nullable=False)
     last_modified = db.Column(db.DateTime, default=func.now(), onupdate=func.now(), nullable=False)
